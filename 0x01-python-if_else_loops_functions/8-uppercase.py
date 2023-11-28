ffd9fe8e1ba7d409e9baef4b0403315d6c75f37f
chessboy71 <bentegrimohamed@gmail.com>
2023-11-28 12:35:55 -0800

    S

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,3 @@ def uppercase(s):
             print('{}'.format(chr(ord(char) - ord('a') + ord('A'))), end='')
         else:
             print('{}'.format(char), end='')
-<<<<<<< HEAD
-=======
-    print()
->>>>>>> dc94a55de663c2012ae5f447070f7448681b45fc
