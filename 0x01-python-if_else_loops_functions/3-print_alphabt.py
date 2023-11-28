a0742b936c3455b00e8df6027a0123872a0730a2
chessboy71 <bentegrimohamed@gmail.com>
2023-11-28 09:49:15 -0800

    s

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if (i == e or i == q):
+    if (i == 'e' or i == 'q'):
         continue
     else:
         print('{}'.format(chr(i)), end='')
