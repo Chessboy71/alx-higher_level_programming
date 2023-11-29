fc4dd0851b75fb2af3f01da9feef1ae7606ea70f
root <root@Yacine>
2023-11-29 21:54:06 +0100

    101 and 102

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (26):
+for i in range(26):
     if (i % 2 == 0):
         n = 'z'
     else:
         n = 'Z'
-    print('{}'.format(chr(ord(n) - i)), end="")   
+    print('{}'.format(chr(ord(n) - i)), end="")
