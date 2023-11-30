742a3b8c35a8592db2d76ae2db696bc859a127d2
chessboy71 <bentegrimohamed@gmail.com>
2023-11-30 18:26:09 +0100

    s

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-import sys
-print ("{} arguments.".format(len(sys.argv) - 1))
-for c, arg in enumerate(sys.argv):
-    if (c > 0):
-        print ("{} : {}".format(c, arg))
+if (__name__ == "__main__"):
+    import sys
+    if (len(sys.argv) == 1):
+        print ("{} arguments.".format(len(sys.argv) - 1))
+    print ("{} arguments:".format(len(sys.argv) - 1))
+    for c, arg in enumerate(sys.argv):
+        if (c > 0):
+            print ("{} : {}".format(c, arg))
 
