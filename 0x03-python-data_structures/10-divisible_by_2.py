80d7464e129b30d0e80b338e8e020423c148a594
chessboy71 <bentegrimohamed@gmail.com>
2023-12-05 16:31:54 +0100

    S

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if (len(my_list) == 0):
+        return None
+    for i in my_list:
+        if (my_list[i] % 2):
+            my_list[i] = True
+        else:
+            my_list[i] = False
+
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return None
     m = my_list[0]
-    for i in range(1, len(my_list) - 1):
+    for i in range(1, len(my_list)):
         m = my_list[i] if m < my_list[i] else m
     return m
