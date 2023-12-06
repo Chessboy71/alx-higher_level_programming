d5c597529f854701ac7fe8c5ac51b1963e7afe56
chessboy71 <bentegrimohamed@gmail.com>
2023-12-06 16:42:32 +0100

    first tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    newl = []
+    for i in my_list:
+        if (i == search):
+            newl.append(replace)
+        else:
+            newl.append(i)
+
+    return newl
