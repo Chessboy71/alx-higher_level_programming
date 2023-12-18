6866380efc67d19849d20e20a95aeb40dd18384b
root <root@Yacine>
2023-12-18 20:56:55 +0100

    s

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        r = 0
         for i in range(x):
             print(my_list[i], end="")
+            r += 1
     except IndexError:
         None
     print()
-    return i
+    return r
