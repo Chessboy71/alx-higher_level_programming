a3753b4865bae2d6ef982afb172950121ed8f9e6
chessboy71 <bentegrimohamed@gmail.com>
2023-12-30 15:54:39 +0100

    s

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,21 +7,6 @@ class Square:
 
     """ initiation function with a private size instance"""
 
-    def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        """ 
-        Raisign errors if the value is not correct
-        """
-
-        if not type(size) is int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        
+    def __init__(self, size):
         self.__size = size
         
-
