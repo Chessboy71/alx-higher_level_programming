d765f0e545dbeb63513eed67d6fcf1be6fd1e365
chessboy71 <bentegrimohamed@gmail.com>
2023-12-30 15:44:36 +0100

    first and second tak

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """class that defines a square"""
+
+
 class Square:
     """the square representation"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,10 @@
 
 """class that defines a square"""
 
+
 class Square:
 
     """ initiation function with a private size instance"""
-    
+
     def __init__(self, size):
         self.__size = size
