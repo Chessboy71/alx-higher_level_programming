b9e0eac5bf93378abbbcee71d9950115636c350a
chessboy71 <bentegrimohamed@gmail.com>
2023-12-30 15:57:25 +0100

    S

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        """ 
+        """
         Raisign errors if the value is not correct
         """
 
@@ -21,5 +21,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
