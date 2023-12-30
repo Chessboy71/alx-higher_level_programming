a193746fa97ae69766e34f8a7102da85a628f236
chessboy71 <bentegrimohamed@gmail.com>
2023-12-30 18:55:54 +0100

    S

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,7 +7,7 @@ class Square:
 
     """ initiation function with a private size instance"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new Square.
 
         Args:
@@ -17,6 +17,7 @@ class Square:
         Raisign errors if the value is not correct
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -29,6 +30,18 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    @property
+    def position(self):
+        return self.__position
+    
+    def position(self, value):
+            if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+                raise TypeError("position must be a tuple of 2 positive integers")
+
+            self.__position = value
 
     def area(self):
         """
@@ -38,10 +51,29 @@ class Square:
 
     def my_print(self):
 
+        for i in range(self.__position[0]):
+            print()
         """ print the square using #"""
         for i in range(self.__size):
+            for j in range(self.__position[1]):
+                print("_", end="")
             for j in range(self.__size):
                 print('#', end="")
             print()
         if self.__size == 0:
             print("")
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
