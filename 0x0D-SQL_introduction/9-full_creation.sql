804385f9cd10dd00a3a3fe4b90528ef094c6783e
chessboy71 <bentegrimohamed@gmail.com>
2024-02-16 00:07:12 +0100

    s

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- the second table creation 
+CREATE TABLE second_table(
+    id INT,
+    name VARCHAR(255),
+    score INT
+);
+INSERT INTO second_table (id, name, score)
+(1 ,"John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
