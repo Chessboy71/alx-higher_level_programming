07099872ba7a5f59b9f48e6dd6d25e8971d99ef1
chessboy71 <bentegrimohamed@gmail.com>
2024-02-16 01:15:12 +0100

    d

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Selecting a number 
-SELECT score, COUNT(*) AS 'number'
-FROM second_table
-GROUP BY score
-ORDER BY 'number' DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
