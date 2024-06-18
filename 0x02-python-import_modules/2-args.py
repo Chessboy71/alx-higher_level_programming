6344b9270f4afbb3ffe56164cd282d71354a09e6
chessboy71 <bentegrimohamed@gmail.com>
2024-06-18 03:31:17 +0200

    mendatory tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+    if len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        print("0 arguments.")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if (__name__ == "__main__"):
+if __name__ == "__main__":
+    res = 0
     import sys
-    result = 0
-    for arg in sys.argv:
-        if (arg != sys.argv[0]):
-            result += int(arg)
-    print("{}".format(result))
+    for i in range(1, len(sys.argv)):
+        res = res + int(sys.argv[i])
+    print(res)
